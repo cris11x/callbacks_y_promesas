1a8a78c13716d8b8aef6e72b8c920b7787c5b18b
Cristian <cris10x@hotmail.com>
2021-02-23 09:05:37 -0500

    Promesas en paralelo o promise.all

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
 import { buscarHeroe} from './js/promesas';
 import './styles.css';
 
-const heroeId1 = 'capi2';
+const heroeId1 = 'capi';
 const heroeId2 = 'spider';
 
 /*buscarHeroe( heroeId1, ( err, heroe1 ) => {
@@ -18,8 +18,20 @@ const heroeId2 = 'spider';
     })
 });*/
 
-buscarHeroe( heroeId1 ).then( heroe => {
-    console.log(`enviando a ${ heroe.nombre } a la mision`);
+/*buscarHeroe( heroeId1 ).then( heroe1 => {
+
+    //console.log(`enviando a ${ heroe.nombre } a la mision`);
+    buscarHeroe( heroeId2 ).then( heroe2 => {
+        console.log(`Enviado a ${ heroe1.nombre } y ${ heroe2.nombre } a la mision`);
+    });
+});*/
+
+//Promesas en paralelo
+
+Promise.all( buscarHeroe(heroeId1), buscarHeroe(heroeId2) )
+    .then( ([ heroe1, heroe2 ]) => {
+
+    console.log(`Enviado a ${ heroe1.nombre } y ${ heroe2.nombre } a la mision`);
 });
 
 console.log(`Fin del programa`);
